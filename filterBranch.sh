e5de4505ca24db469206f7df8205e907f56a42b2
Gary <gnewsome@amplience,com>
2020-03-23 15:07:05 +0000

    no message

--- filterBranch.sh
+++ filterBranch.sh
@@ -1,1 +1,1 @@
-git filter-branch --force --index-filter "git rm --cached --ignore-unmatch 'General-Tooling/checkCFStacks.sh'" --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter "git rm --cached --ignore-unmatch 'CheckCFStacks.sh'" --prune-empty --tag-name-filter cat -- --all
