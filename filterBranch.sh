e8e1e9bd85951c1273217ed312e60110d437fae0
Gary <gnewsome@amplience,com>
2020-03-23 15:00:48 +0000

    test commit 3

--- filterBranch.sh
+++ filterBranch.sh
@@ -1,3 +1,3 @@
 git filter-branch --force --index-filter \
-  "git rm --cached --ignore-unmatch 'https://github.com/gnewso2909/General-Tooling/commit/1fdb17b45938af398dd4ac1f8d456bd4c2ec64d7'" \
+  "git rm --cached --ignore-unmatch 'https://github.com/gnewso2909/General-Tooling/checkCFStacks.sh '" \
   --prune-empty --tag-name-filter cat -- --all
